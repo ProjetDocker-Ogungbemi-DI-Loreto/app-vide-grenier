7d22d5fd7399577cee47ec18d1af92fb06299f04
Romain DI LORETO <romain.diloreto26@gmail.com>
2024-09-26 10:17:42 +0200

    attendre à la fin de deploy.sh pour l'init de la base

--- www/scripts/deploy.sh
+++ www/scripts/deploy.sh
@@ -34,13 +34,17 @@ cp $ENV_FILE .env
 
 if [ $BRANCH == "dev" ]; then
   sed -i '/^\s*root\s*/c\    root /var/www/vide-grenier-dev/public;' .docker/nginx/site.template
+  DB_CONTAINER=vide-grenier-dev_db
 elif [ $BRANCH == "stage" ]; then
   sed -i '/^\s*root\s*/c\    root /var/www/vide-grenier-stage/public;' .docker/nginx/site.template
+  DB_CONTAINER=vide-grenier-stage_db
 elif [ $BRANCH == "main" ]; then
   sed -i '/^\s*root\s*/c\    root /var/www/vide-grenier-main/public;' .docker/nginx/site.template
+  DB_CONTAINER=vide-grenier-main_db
 else
   echo "Unknown branch: $BRANCH"
   sed -i '/^\s*root\s*/c\    root /var/www/vide-grenier/public;' .docker/nginx/site.template
+  DB_CONTAINER=vide-grenier_db
 fi
 
 # Load the environment variables and run docker-compose
@@ -48,7 +52,9 @@ export $(grep -v '^#' $ENV_FILE | xargs)
 docker compose down -v
 docker compose -f docker-compose.yml up -d --build
 
-docker cp ./www/scripts/restore_db.sh  vide-grenier-$BRANCH_db:/restore_db.sh
-docker cp ./www/scripts/restore_db.sh  vide-grenier-$BRANCH_db:/dump_db.sh
-docker cp ./www/sql/db-vide-grenier.sql vide-grenier-$BRANCH_db:/init.sql
-docker exec -it vide-grenier-dev_$BRANCH_db sh -c "/restore_db.sh /init.sql"
+sleep 20
+
+docker cp ./www/scripts/restore_db.sh  $DB_CONTAINER:/restore_db.sh
+docker cp ./www/scripts/restore_db.sh  $DB_CONTAINER:/dump_db.sh
+docker cp ./www/sql/db-vide-grenier.sql $DB_CONTAINER:/init.sql
+docker exec -it $DB_CONTAINER sh -c "/restore_db.sh /init.sql"
